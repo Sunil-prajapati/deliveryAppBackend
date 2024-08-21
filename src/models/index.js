e65ac22e3ecbf5c821af8863830d847f0cd4926f
Sunil Prajapati <sunil.prajapati@statrys.com>
2024-08-21 23:50:16 +0530

    added order schema and added auth middleware

--- src/models/index.js
+++ src/models/index.js
@@ -1,6 +1,17 @@
 import Branch from "./branch.js";
 import Category from "./category.js";
+import Counter from "./counter.js";
+import Order from "./order.js";
 import Product from "./products.js";
 import { Customer, DeliveryPartner, Admin } from "./user.js";
 
-export { Branch, Customer, DeliveryPartner, Admin, Product, Category };
+export {
+  Branch,
+  Customer,
+  DeliveryPartner,
+  Admin,
+  Product,
+  Category,
+  Counter,
+  Order,
+};
